463445f35a5d26f5115e08b4f9cbdf9adf08f741
Racteja <151444929+Racteja@users.noreply.github.com>
2024-01-08 20:11:57 +0530

    Add files via upload

---
+++ StudentInformation.java
@@ -0,0 +1,16 @@
+public class StudentInformation{
+        public static void main(String[] args){
+                printStudentInfo();
+        }
+        private static void printStudentInfo(){
+                string collegeName="AITS";
+                string studentName="KRISHNA";
+                int rollNumber=1596;
+                System.out.println("college name:"+collegeName);
+                System.out.println("student Name:"+studentName);
+                System.out.println("Roll Number:"+rollNumber);
+                System.out.println("\nwelcome,"+studentName+"!");
+                System.out.println("we hope you have a fantastic journey at "+collegeName+".");
+                System.out.println("KILL THE SLEEPING PEOPLE");
+        }
+}
